8360ac5b08806d33d452a8082e022ab13e03daac
mimmo31 <crissbecerra31032003@gmail.com>
2025-08-28 14:18:30 -0500

    cambio_9_VistaHomeyTemplateUseryUserController_mimmo31

---
+++ src/main/java/com/sena/ejercicio1/controller/UsuarioContoller.java
@@ -0,0 +1,16 @@
+package com.sena.ejercicio1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UsuarioContoller {
+
+	@GetMapping("")
+	public String home() {
+		return "usuario/home";
+	}
+
+}
